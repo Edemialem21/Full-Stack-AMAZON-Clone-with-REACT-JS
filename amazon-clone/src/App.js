fa2b4a2f36cbfae829784dc42cbd58f7247c738e
edemialem1 <153983897+edemialem1@users.noreply.github.com>
2023-12-25 09:05:36 +0000

    amazon header

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  return (
+    // 
+    <div className="App">
+      <Header />
+      <Home />
+      <h1>Hello, it's Edemialem, let's build amazon</h1>
+      
+      {/*header*/}
+      {/**home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCh9LnM6eq_AKLzQI63Fwja7nN32c5ab38",
+    authDomain: "clone-fde98.firebaseapp.com",
+    projectId: "clone-fde98",
+    storageBucket: "clone-fde98.appspot.com",
+    messagingSenderId: "196970104426",
+    appId: "1:196970104426:web:64c65a931f5b7432a8ec13",
+    measurementId: "G-5J282506DZ"
+  };
