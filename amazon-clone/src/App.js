01753442eb99697590a6c91b4869262c1cdb67a0
edemialem1 <153983897+edemialem1@users.noreply.github.com>
2023-12-26 10:40:57 +0000

    amazon home part with router and swich

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    // 
     <div className="App">
-      <Header />
-      <Home />
+
+      <Router>
+          <Routes>
+            <Route path='/'>
+             <Header />
+             <Home />
+             </Route>
+           </Routes>
+         </Router>
+      
       <h1>Hello, it's Edemialem, let's build amazon</h1>
       
       {/*header*/}
