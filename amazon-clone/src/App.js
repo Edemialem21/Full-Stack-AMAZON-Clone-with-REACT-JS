85cb1d479f32a7fbed4296073157c6755143169d
edemialem1 <153983897+edemialem1@users.noreply.github.com>
2023-12-27 05:51:50 +0000

    bu fixied

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -5,22 +5,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-
-      <Router>
-          <Routes>
-            <Route path='/'>
              <Header />
              <Home />
-             </Route>
-           </Routes>
-         </Router>
       
       <h1>Hello, it's Edemialem, let's build amazon</h1>
       
       {/*header*/}
       {/**home */}
     </div>
+    </Router>
   );
 }
 
