36311c47b142df2ba39bdc2cb9648368d622d3ac
Edemialem21 <21edemialem@gmail.com>
2023-12-29 11:00:52 +0300

    products and produts info with their prie

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import Header from './componets/Header';
 import Home from './componets/Home';
-import Layout from "./componets/Layout";
-import Contact from "./componets/Contact";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
         <div className="App">
             <Header />
+            <Router>
                 <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="contacts" element={<Contact />} />
+                    <Route path="/" element={<Home />}>
                     </Route>
                 </Routes>
+            </Router>
       <h1>Hello, it's Edemialem, let's build amazon</h1>
         </div>
-    </BrowserRouter>
+   
   );
 }
 
